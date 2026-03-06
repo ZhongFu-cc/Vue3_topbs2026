b7c3e04dbde6a2f78319af796a40d0d8287aa0ce
youjiayou <look53486@gmail.com>
2026-03-06 16:36:03 +0800

    update: update year

--- src/settings.ts
+++ src/settings.ts
@@ -7,33 +7,7 @@ const { pkg } = __APP_INFO__;
 
 const defaultSettings: AppSettings = {
   // title: pkg.name,
-  title: "TICBCS 2025 後台",
-  version: pkg.version,
-  showSettings: true,
-  tagsView: false,
-  fixedHeader: true,
-  sidebarLogo: true,
-  layout: LayoutEnum.LEFT,
-  theme: ThemeEnum.LIGHT,
-  size: SizeEnum.DEFAULT,
-  language: LanguageEnum.ZH_CN,
-  themeColor: "#409EFF",
-  watermarkEnabled: false,
-  watermarkContent: pkg.name,
-};
-
-export default defaultSettings;
-
-import { SizeEnum } from "./enums/SizeEnum";
-import { LayoutEnum } from "./enums/LayoutEnum";
-import { ThemeEnum } from "./enums/ThemeEnum";
-import { LanguageEnum } from "./enums/LanguageEnum";
-
-const { pkg } = __APP_INFO__;
-
-const defaultSettings: AppSettings = {
-  // title: pkg.name,
-  title: "TOPBS 2025 後台",
+  title: "TICBCS 2026 後台",
   version: pkg.version,
   showSettings: true,
   tagsView: false,
